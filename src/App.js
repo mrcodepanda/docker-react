55a839d025f8c847de4ff22e307a69fc438f258b
Sudhanshu Aggarwal <codepanda@pop-os.lan>
2024-03-21 01:04:01 -0700

    change text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          new text
+          I was changed in feature branch!
         </p>
       </div>
     );
